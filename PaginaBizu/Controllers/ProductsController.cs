ae46000a233b83859114aa2e273a37a27cf7c48e
OsakaThx <hoshuacastillo48@gmail.com>
2025-08-16 17:46:03 -0600

    Subir imagenes por medio de archivos

--- PaginaBizu/Controllers/ProductsController.cs
+++ PaginaBizu/Controllers/ProductsController.cs
@@ -1,152 +1,224 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PaginaBizu.Data;
 using PaginaBizu.Models;
-
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace PaginaBizu.Controllers
 {
-	[Authorize(Roles = "Admin")]
-	public class ProductsController : Controller
-	{
-		private readonly AppDbContext _context;
+    [Authorize(Roles = "Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-		public ProductsController(AppDbContext context)
-		{
-			_context = context;
-		}
+        public ProductsController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
 
-		// GET: Products
-		public async Task<IActionResult> Index()
-		{
-			return View(await _context.Products.ToListAsync());
-		}
-		// GET: Products/Details/5
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-				return NotFound();
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
 
-			var product = await _context.Products
-				.FirstOrDefaultAsync(p => p.Id == id);
+            var product = await _context.Products
+                .FirstOrDefaultAsync(p => p.Id == id);
 
-			if (product == null)
-				return NotFound();
+            if (product == null)
+                return NotFound();
 
-			// Retorna una vista que solo muestre detalles administrativos sin comentarios
-			return View(product);
-		}
+            // Retorna una vista que solo muestre detalles administrativos sin comentarios
+            return View(product);
+        }
 
-		// GET: Products/Create
-		public IActionResult Create()
-		{
-			return View();
-		}
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-		// POST: Products/Create
-		
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("Id,NombreArticulo,CantidadDisponible,Precio,Descripcion,ImagenUrl,Categoria,FechaCreacion")] Product product)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Add(product);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			return View(product);
-		}
+        // POST: Products/Create
+        
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NombreArticulo,CantidadDisponible,Precio,Descripcion,ImagenUrl,Categoria,FechaCreacion")] Product product, IFormFile imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    var uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "products");
+                    
+                    // Create directory if it doesn't exist
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
 
-		// GET: Products/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+                    // Generate a unique file name
+                    var uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(imageFile.FileName);
+                    var filePath = Path.Combine(uploadsFolder, uniqueFileName);
 
-			var product = await _context.Products.FindAsync(id);
-			if (product == null)
-			{
-				return NotFound();
-			}
-			return View(product);
-		}
+                    // Save the file
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await imageFile.CopyToAsync(fileStream);
+                    }
 
-		// POST: Products/Edit/5
+                    // Update the image URL
+                    product.ImagenUrl = $"/images/products/{uniqueFileName}";
+                }
 
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("Id,NombreArticulo,CantidadDisponible,Precio,Descripcion,ImagenUrl,Categoria,FechaCreacion")] Product product)
-		{
-			if (id != product.Id)
-			{
-				return NotFound();
-			}
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(product);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!ProductExists(product.Id))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			return View(product);
-		}
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-		// GET: Products/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
 
-			var product = await _context.Products
-				.FirstOrDefaultAsync(m => m.Id == id);
-			if (product == null)
-			{
-				return NotFound();
-			}
+        // POST: Products/Edit/5
 
-			return View(product);
-		}
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NombreArticulo,CantidadDisponible,Precio,Descripcion,ImagenUrl,Categoria,FechaCreacion")] Product product, IFormFile imageFile)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
 
-		// POST: Products/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			var product = await _context.Products.FindAsync(id);
-			if (product != null)
-			{
-				_context.Products.Remove(product);
-			}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (imageFile != null && imageFile.Length > 0)
+                    {
+                        var uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "products");
+                        
+                        // Create directory if it doesn't exist
+                        if (!Directory.Exists(uploadsFolder))
+                        {
+                            Directory.CreateDirectory(uploadsFolder);
+                        }
 
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
+                        // Delete old image if it exists
+                        if (!string.IsNullOrEmpty(product.ImagenUrl))
+                        {
+                            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.ImagenUrl.TrimStart('/').Replace('/', '\\'));
+                            if (System.IO.File.Exists(oldImagePath))
+                            {
+                                System.IO.File.Delete(oldImagePath);
+                            }
+                        }
 
-		private bool ProductExists(int id)
-		{
-			return _context.Products.Any(e => e.Id == id);
-		}
-	}
+                        // Generate a unique file name
+                        var uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(imageFile.FileName);
+                        var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                        // Save the new file
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await imageFile.CopyToAsync(fileStream);
+                        }
+
+                        // Update the image URL
+                        product.ImagenUrl = $"/images/products/{uniqueFileName}";
+                    }
+
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                // Delete the associated image file if it exists
+                if (!string.IsNullOrEmpty(product.ImagenUrl))
+                {
+                    var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.ImagenUrl.TrimStart('/').Replace('/', '\\'));
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                }
+
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
 }
--- PaginaBizu/obj/Release/net8.0/PaginaBizu.AssemblyInfo.cs
+++ PaginaBizu/obj/Release/net8.0/PaginaBizu.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaBizu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0401688186656868148ac9ebecb75defee4a962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fa70db70aad413e4950eaf76417945fad03477")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaBizu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaBizu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
